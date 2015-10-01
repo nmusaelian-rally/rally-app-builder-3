b7c8ad80113135e59cd90f2f0864a81642daa56a
Komran Rashidov <krashidov@rallydev.com>
2015-10-01 17:49:22 -0600

    npm run build:webpack now builds an index.html that is copy and pasteable. Remaining TODO: fix npm run clean and tell user about the file

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,21 @@
 var path = require('path');
 var webpack = require('webpack');
 
+var BABEL_CACHE_TEMP_DIR = path.resolve(__dirname, './.temp/babel');
+var version = require('./package.json').version;
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+HtmlWebpackPlugin.prototype.emitHtml = function(compilation, htmlTemplateContent, templateParams, outputFilename) {
+  compilation.assets[outputFilename] = {
+    source: function() {
+      return htmlTemplateContent;
+    },
+    size: function() {
+      return htmlTemplateContent.length;
+    }
+  };
+};
+
 module.exports = {
   devtool: 'source-map',
   entry: [
@@ -22,13 +37,27 @@ module.exports = {
       compressor: {
         warnings: false
       }
-    })
+    }),
+    new HtmlWebpackPlugin({
+      templateContent: function(templateParams, compilation) {
+        var code = compilation.assets['bundle.js'].children[0]._value;
+
+        return ['<!DOCTYPE html> <html><title>My Rally App</title> <head> <script type="text/javascript">',
+                code,
+                '</script><meta http-equiv="Content-type" content="text/html; charset=utf-8"/>  </head> <body> </body> </html>'].join('');
+      }
+    })  
   ],
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
+    },{
+      test: /\.js$/,
+      loader: 'babel-loader?stage=1&optional=runtime&loose=es6.classes&cacheDirectory=' + BABEL_CACHE_TEMP_DIR + '&cacheIdentifier=' + version,
+      devLoader: 'babel-loader?stage=1&optional=runtime&loose=es6.classes&cacheDirectory=' + BABEL_CACHE_TEMP_DIR + '&cacheIdentifier=' + version,
+      exclude: /node_modules\/(?!sombrero)/
     },
     {
       test: /\.scss$/,
@@ -46,6 +75,22 @@ module.exports = {
     {
       test: /\.png/,
       loader: 'url?limit=10000&minetype=image/png'
+    },
+    {
+      test: /\.woff$/,
+      loader: "url?limit=10000&minetype=application/font-woff"
+    },
+    {
+      test: /\.ttf$/,
+      loader: "file"
+    },
+    {
+      test: /\.eot$/,
+      loader: "file"
+    },
+    {
+      test: /\.svg$/,
+      loader: "file"
     }]
   }
 };
